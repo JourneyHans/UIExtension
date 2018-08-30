d4c669fe1cc2767e0f2fbdfc1fa32ba53e011fd1
JourneyHans <2749628228@qq.com>
2018-08-30 17:38:36 +0800

    [Add] 增加SpriteChanger

---
+++ Assets/UIExtension/SpriteChanger/SpriteChanger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpriteChanger : MonoBehaviour
+{
+    public List<Sprite> ImageList;      // 图片列表
+    public bool AutoNativeSize;         // 是否自适应大小
+
+    private Image _currentImage;
+    public int Index{ set; get; }
+
+    void Start()
+    {
+        _currentImage = GetComponent<Image>();
+//        _currentImage.sprite = ImageList[0];
+        ChangeToIndex(0);
+    }
+
+    public void ChangeToIndex(int idx)
+    {
+        Index = idx;
+        _currentImage.sprite = ImageList[idx];
+        if (AutoNativeSize)
+        {
+            _currentImage.SetNativeSize();
+        }
+    }
+
+    public int GetSpriteListLength()
+    {
+        return ImageList.Count;
+    }
+}
---
+++ Assets/UIExtension/SpriteChanger/SpriteChangerTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpriteChangerTest : MonoBehaviour
+{
+    public GameObject spriteChangerGO;
+
+    private SpriteChanger spriteChanger;
+    private float lastUpdateTime;
+    private int index;
+    void Start()
+    {
+        spriteChangerGO = Instantiate(spriteChangerGO);
+        spriteChangerGO.transform.SetParent(transform, false);
+        spriteChanger = spriteChangerGO.GetComponent<SpriteChanger>();
+    }
+
+    void Update()
+    {
+        if (Time.time - lastUpdateTime >= 1f)
+        {
+            index++;
+            spriteChanger.ChangeToIndex(index % spriteChanger.GetSpriteListLength());
+            Debug.Log("CurrentIndex: " + spriteChanger.Index);
+            lastUpdateTime = Time.time;
+        }
+    }
+}
