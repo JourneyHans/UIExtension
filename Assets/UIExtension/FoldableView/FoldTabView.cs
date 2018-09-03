1ef762ace71fcfa31aadd08f377ce5c50472b575
JourneyHans <2749628228@qq.com>
2018-09-03 11:22:24 +0800

    [Fixed] SpriteChanger相关初始化放在Awake里

---
+++ Assets/UIExtension/FoldableView/FoldTabView.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class FoldTabView : ScrollRect
+{
+    [SerializeField]
+    public GameObject FatherTabPrefab;
+
+    [SerializeField]
+    public GameObject ChildTabPrefab;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        
+    }
+
+    private void CheckState()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
--- Assets/UIExtension/SpriteChanger/SpriteChanger.cs
+++ Assets/UIExtension/SpriteChanger/SpriteChanger.cs
@@ -8,16 +8,18 @@ public class SpriteChanger : MonoBehaviour
     public List<Sprite> ImageList;      // 图片列表
     public bool AutoNativeSize;         // 是否自适应大小
 
-    private Image _currentImage;
-    public int Index{ set; get; }
+    private Image _currentImage;        // 当前显示的Image
+    public int Index{ set; get; }       // 当前索引
 
-    void Start()
+    void Awake()
     {
         _currentImage = GetComponent<Image>();
-//        _currentImage.sprite = ImageList[0];
         ChangeToIndex(0);
     }
 
+    /// <summary>
+    /// 根据下标切换图片，下标从0开始
+    /// </summary>
     public void ChangeToIndex(int idx)
     {
         Index = idx;
@@ -28,6 +30,9 @@ public class SpriteChanger : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// 获取图片个数
+    /// </summary>
     public int GetSpriteListLength()
     {
         return ImageList.Count;
